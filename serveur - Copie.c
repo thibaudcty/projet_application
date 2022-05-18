fcebb4f1b9225ac8053c3438debf58e95667d837
thibaudcty <thibaud.couty@insa-cvl.fr>
2022-05-18 14:05:47 +0200

    back to the basics

---
+++ serveur - Copie.c
@@ -0,0 +1,212 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+   
+   write_ip();    
+    
+}
+void write_ip(int argc, char *argv[]){
+    int listenfd = 0;
+    int connfd = 0;
+    struct sockaddr_in serv_addr = {0};
+    // Le buffer pour envoyer les données
+    char sendBuff[1025] = {0};
+    
+    
+    // Création de la socket serveur
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    //Initialisation de la structure sockaddr
+    serv_addr.sin_family = AF_INET;
+    //Accepte les connexions depuis n'importe quelle adresse
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    // Le port sur lequel la socket va écouter
+    serv_addr.sin_port = htons(7000);
+     bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    
+    //La socket écoute pour des connexions
+    listen(listenfd, 10);
+    char hostname[128];
+    gethostname(hostname, sizeof hostname);
+    int pid = 0;
+    while(1)
+    {
+        // Accepte la connexion d'une socket client
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+        
+        // Exécution d'un fork pour gérer la connexion
+        if((pid=fork())==-1) {
+            printf("erreur\n");
+            close(connfd);
+        }
+        else if(pid>0) { // Le processus père
+            close(connfd);
+        }
+        else if(pid==0) { // Le processus fils
+            snprintf(sendBuff, sizeof(sendBuff), "%s\n", hostname);
+            write(connfd, sendBuff, strlen(sendBuff));
+            
+            close(connfd);
+        }
+    }
+}
+void read_ip(int argc, char *argv[]){
+    char recvBuff[1024] = {0};
+    int sockfd=0;
+    int n=0;
+    struct sockaddr_in serv_addr = {0};
+    /*
+     * Si l'IP du serveur n'a pas été passée en argument
+     * le programme se termine
+     */
+    if(argc != 2)
+    {
+        printf("\n Usage: %s <ip of server> \n",argv[0]);
+        return 1;
+    }
+    
+    // Création de la socket
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Error : Could not create socket \n");
+        return 1;
+    }
+    
+    serv_addr.sin_family = AF_INET;
+    // Le port sur lequel écoute le serveur
+    serv_addr.sin_port = htons(7000);
+    
+    // Copie l'adresse ip du serveur dans la structure serv_addr
+    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
+    {
+        printf("\n inet_pton error occured\n");
+        return 1;
+    }
+    
+    // Connection au serveur
+    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\n Error : Connect Failed \n");
+        return 1;
+    }
+   
+    char Clients[10][1];
+    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
+    {
+        recvBuff[n] = 0;
+        // Affichage des informations recues sur la sortie standard
+        if(fputs(recvBuff, stdout) == EOF)
+        {
+            printf("\n Error : Fputs error\n");
+        }
+
+        
+        
+       
+        entrerValTab(Clients,argv[1],recvBuff);
+        modifText(Clients);
+  }
+
+    if(n < 0)
+    {
+        printf("\n Read error \n");
+    }
+    return 0;	   
+ }
+
+void modifText(char tab[int n][int m]){
+    FILE *fichier =NULL;
+    fichier= fopen("ClientsList.txt", "w+");
+    for(i=0; i<n; i++){
+        fprintf(fichier, tab[i][0], tab[i][1]);
+        fprintf(fichier, "\n");
+        fclose(fichier);
+    }
+}
+
+void entrerValTab(char tab[n][m],char a, char b){
+    for (i = 0; i<n; i++){
+        if(tab[i][0] == NULL){
+            pos = i;
+        }
+    }
+    tab[pos][0]= a;
+    tab[pos][1]= b;
+}
+char* getadresse(){
+        //create an ifreq struct for passing data in and out of ioctl
+        struct ifreq my_struct;
+     
+        //declare and define the variable containing the name of the interface
+        char *interface_name="ens33";   //a very frequent interface name is "eth0";
+     
+        //the ifreq structure should initially contains the name of the interface to be queried. Which should be copied into the ifr_name field.
+        //Since this is a fixed length buffer, one should ensure that the name does not cause an overrun
+        size_t interface_name_len=strlen(interface_name);
+     
+        if(interface_name_len<sizeof(my_struct.ifr_name))
+        {
+            memcpy(my_struct.ifr_name,interface_name,interface_name_len);
+            my_struct.ifr_name[interface_name_len]=0;
+        }
+        else
+        {
+            perror("Copy name of interface to ifreq struct");
+            printf("The name you provided for the interface is too long...\n");
+        }
+     
+        //provide an open socket descriptor with the address family AF_INET
+        /* ***************************************************************
+         * All ioctl call needs a file descriptor to act on. In the case of SIOCGIFADDR this must refer to a socket file descriptor. This socket must be in the address family that you wish to obtain (AF_INET for IPv4)
+         * ***************************************************************
+         */
+     
+        int file_descriptor=socket(AF_INET, SOCK_DGRAM,0);
+     
+        if(file_descriptor==-1)
+        {
+            perror("Socket file descriptor");
+            printf("The construction of the socket file descriptor was unsuccessful.\n");
+            return -1;
+        }
+     
+        //invoke ioctl() because the socket file descriptor exists and also the struct 'ifreq' exists
+        int myioctl_call=ioctl(file_descriptor,SIOCGIFADDR,&my_struct);
+     
+        if (myioctl_call==-1)
+        {
+            perror("ioctl");
+            printf("Ooops, error when invoking ioctl() system call.\n");
+            close(file_descriptor);
+            return -1;
+        }
+     
+        close(file_descriptor);
+     
+        /* **********************************************************************
+         * If this completes without error , then the hardware address of the interface should have been returned in the  'my_struct.ifr_addr' which is types as struct sockaddr_in.
+         * ***********************************************************************/
+     
+      //extract the IP Address (IPv4) from the my_struct.ifr_addr which has the type 'ifreq'
+     
+        /* *** Cast the returned address to a struct 'sockaddr_in' *** */
+        struct sockaddr_in * ipaddress= (struct sockaddr_in *)&my_struct.ifr_addr;
+       /* *** Extract the 'sin_addr' field from the data type (struct) to obtain a struct 'in_addr' *** */
+      printf("IP Address is %s.\n", inet_ntoa(ipaddress->sin_addr));
+      return inet_ntoa(ipaddress->sin_addr);
+}
+ void inscrire(int fd){
+     char* ip=getadresse();
+     char buffer[1024];
+     send(fd, ip, strlen(ip),0);
+ }
--- serveur.c
+++ serveur.c
@@ -1,40 +1,40 @@
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <netdb.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <time.h>
+#include <arpa/inet.h>
 
 int main(int argc, char *argv[])
 {
-   
-   write_ip();    
     
+    read_ip();
+       
 }
-void write_ip(int argc, char *argv[]){
+void write_ip(){
     int listenfd = 0;
     int connfd = 0;
     struct sockaddr_in serv_addr = {0};
-    // Le buffer pour envoyer les donnÃ©es
+    // Le buffer pour envoyer les données
     char sendBuff[1025] = {0};
     
     
-    // CrÃ©ation de la socket serveur
+    // Création de la socket serveur
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     
     //Initialisation de la structure sockaddr
     serv_addr.sin_family = AF_INET;
     //Accepte les connexions depuis n'importe quelle adresse
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    // Le port sur lequel la socket va Ã©couter
+    // Le port sur lequel la socket va écouter
     serv_addr.sin_port = htons(7000);
      bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
     
-    //La socket Ã©coute pour des connexions
+    //La socket écoute pour des connexions
     listen(listenfd, 10);
     char hostname[128];
     gethostname(hostname, sizeof hostname);
@@ -44,12 +44,12 @@ void write_ip(int argc, char *argv[]){
         // Accepte la connexion d'une socket client
         connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
         
-        // ExÃ©cution d'un fork pour gÃ©rer la connexion
+        // Exécution d'un fork pour gérer la connexion
         if((pid=fork())==-1) {
             printf("erreur\n");
             close(connfd);
         }
-        else if(pid>0) { // Le processus pÃ¨re
+        else if(pid>0) { // Le processus père
             close(connfd);
         }
         else if(pid==0) { // Le processus fils
@@ -59,14 +59,17 @@ void write_ip(int argc, char *argv[]){
             close(connfd);
         }
     }
+
+
 }
+
 void read_ip(int argc, char *argv[]){
     char recvBuff[1024] = {0};
     int sockfd=0;
     int n=0;
     struct sockaddr_in serv_addr = {0};
     /*
-     * Si l'IP du serveur n'a pas Ã©tÃ© passÃ©e en argument
+     * Si l'IP du serveur n'a pas été passée en argument
      * le programme se termine
      */
     if(argc != 2)
@@ -75,7 +78,7 @@ void read_ip(int argc, char *argv[]){
         return 1;
     }
     
-    // CrÃ©ation de la socket
+    // Création de la socket
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Error : Could not create socket \n");
@@ -83,7 +86,7 @@ void read_ip(int argc, char *argv[]){
     }
     
     serv_addr.sin_family = AF_INET;
-    // Le port sur lequel Ã©coute le serveur
+    // Le port sur lequel écoute le serveur
     serv_addr.sin_port = htons(7000);
     
     // Copie l'adresse ip du serveur dans la structure serv_addr
@@ -100,7 +103,7 @@ void read_ip(int argc, char *argv[]){
         return 1;
     }
    
-    char Clients[10][1];
+
     while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
     {
         recvBuff[n] = 0;
@@ -110,103 +113,25 @@ void read_ip(int argc, char *argv[]){
             printf("\n Error : Fputs error\n");
         }
 
-        
-        
-       
-        entrerValTab(Clients,argv[1],recvBuff);
-        modifText(Clients);
-  }
+        FILE* fichier = NULL;
+ 
+        fichier = fopen("ServeursList.txt", "a");
+ 
+        if (fichier != NULL)
+            {
+            fprintf(fichier, "\n");
+            fprintf(fichier,"nom et addresse du serveur: %s", recvBuff);
+	    
+            fclose(fichier);
+            }
 
+
+
+    }
+    
     if(n < 0)
     {
         printf("\n Read error \n");
     }
-    return 0;	   
- }
-
-void modifText(char tab[int n][int m]){
-    FILE *fichier =NULL;
-    fichier= fopen("ClientsList.txt", "w+");
-    for(i=0; i<n; i++){
-        fprintf(fichier, tab[i][0], tab[i][1]);
-        fprintf(fichier, "\n");
-        fclose(fichier);
-    }
-}
-
-void entrerValTab(char tab[n][m],char a, char b){
-    for (i = 0; i<n; i++){
-        if(tab[i][0] == NULL){
-            pos = i;
-        }
-    }
-    tab[pos][0]= a;
-    tab[pos][1]= b;
-}
-char* getadresse(){
-        //create an ifreq struct for passing data in and out of ioctl
-        struct ifreq my_struct;
-     
-        //declare and define the variable containing the name of the interface
-        char *interface_name="ens33";   //a very frequent interface name is "eth0";
-     
-        //the ifreq structure should initially contains the name of the interface to be queried. Which should be copied into the ifr_name field.
-        //Since this is a fixed length buffer, one should ensure that the name does not cause an overrun
-        size_t interface_name_len=strlen(interface_name);
-     
-        if(interface_name_len<sizeof(my_struct.ifr_name))
-        {
-            memcpy(my_struct.ifr_name,interface_name,interface_name_len);
-            my_struct.ifr_name[interface_name_len]=0;
-        }
-        else
-        {
-            perror("Copy name of interface to ifreq struct");
-            printf("The name you provided for the interface is too long...\n");
-        }
-     
-        //provide an open socket descriptor with the address family AF_INET
-        /* ***************************************************************
-         * All ioctl call needs a file descriptor to act on. In the case of SIOCGIFADDR this must refer to a socket file descriptor. This socket must be in the address family that you wish to obtain (AF_INET for IPv4)
-         * ***************************************************************
-         */
-     
-        int file_descriptor=socket(AF_INET, SOCK_DGRAM,0);
-     
-        if(file_descriptor==-1)
-        {
-            perror("Socket file descriptor");
-            printf("The construction of the socket file descriptor was unsuccessful.\n");
-            return -1;
-        }
-     
-        //invoke ioctl() because the socket file descriptor exists and also the struct 'ifreq' exists
-        int myioctl_call=ioctl(file_descriptor,SIOCGIFADDR,&my_struct);
-     
-        if (myioctl_call==-1)
-        {
-            perror("ioctl");
-            printf("Ooops, error when invoking ioctl() system call.\n");
-            close(file_descriptor);
-            return -1;
-        }
-     
-        close(file_descriptor);
-     
-        /* **********************************************************************
-         * If this completes without error , then the hardware address of the interface should have been returned in the  'my_struct.ifr_addr' which is types as struct sockaddr_in.
-         * ***********************************************************************/
-     
-      //extract the IP Address (IPv4) from the my_struct.ifr_addr which has the type 'ifreq'
-     
-        /* *** Cast the returned address to a struct 'sockaddr_in' *** */
-        struct sockaddr_in * ipaddress= (struct sockaddr_in *)&my_struct.ifr_addr;
-       /* *** Extract the 'sin_addr' field from the data type (struct) to obtain a struct 'in_addr' *** */
-      printf("IP Address is %s.\n", inet_ntoa(ipaddress->sin_addr));
-      return inet_ntoa(ipaddress->sin_addr);
-}
- void inscrire(int fd){
-     char* ip=getadresse();
-     char buffer[1024];
-     send(fd, ip, strlen(ip),0);
+    return 0;
  }
