67d14f8b0a4eec96dbd04e6fd71ccd2993018283
thibaudcty <thibaud.couty@insa-cvl.fr>
2022-05-20 08:35:25 +0200

    lknlkn

--- client.c
+++ client.c
@@ -120,7 +120,7 @@ void read_ip(int argc, char *argv[]){
         if (fichier != NULL)
             {
             fprintf(fichier, "\n");
-            fprintf(fichier,"nom et addresse du serveur: %s", recvBuff);
+            fprintf(fichier,"adresse at nom :%s , %s",argv[1], recvBuff);
 	    
             fclose(fichier);
             }
